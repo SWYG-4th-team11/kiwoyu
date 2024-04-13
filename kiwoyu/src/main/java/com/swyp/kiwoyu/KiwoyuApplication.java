652806a1b74c38e2bb89ecfa29317adb8c18f8fc
Choihohee <lovelove4927@naver.com>
2024-04-14 01:29:10 +0900

    first commit

---
+++ kiwoyu/src/main/java/com/swyp/kiwoyu/KiwoyuApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.kiwoyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwoyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiwoyuApplication.class, args);
+	}
+
+}
