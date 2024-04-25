470d63e957f9b44d92313319262bc9ea1f7d98f9
primaustryn <bingo13@snu.ac.kr>
2024-04-26 02:02:32 +0900

    feat: initialize configs for swagger and securities

--- kiwoyu/src/main/java/com/swyp/kiwoyu/KiwoyuApplication.java
+++ kiwoyu/src/main/java/com/swyp/kiwoyu/KiwoyuApplication.java
@@ -2,8 +2,12 @@ package com.swyp.kiwoyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages={"com.swyp.kiwoyu"})
 public class KiwoyuApplication {
 
 	public static void main(String[] args) {
