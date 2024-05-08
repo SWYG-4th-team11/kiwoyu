8db13e19cee118f3b692abb8445f6db38fc08366
primaustryn <bingo13@snu.ac.kr>
2024-05-09 00:34:41 +0900

    feat: impl. password encoding logic

--- kiwoyu/src/main/java/com/swyp/kiwoyu/global/common/BaseEntity.java
+++ kiwoyu/src/main/java/com/swyp/kiwoyu/global/common/BaseEntity.java
@@ -1,13 +1,18 @@
 package com.swyp.kiwoyu.global.common;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public abstract class BaseEntity {
 
     @Temporal(TemporalType.TIMESTAMP)
