8dca3f7f2c6a780bf9d22c02c625306707b66048
primaustryn <bingo13@snu.ac.kr>
2024-05-14 19:48:17 +0900

    add mypage dto

---
+++ kiwoyu/src/main/java/com/swyp/kiwoyu/user/dto/MyPageInfoDto.java
@@ -0,0 +1,38 @@
+package com.swyp.kiwoyu.user.dto;
+
+import com.swyp.kiwoyu.mandalart.domain.Mandalart;
+import com.swyp.kiwoyu.user.domain.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MyPageInfoDto {
+    private Long id;
+    private String email;
+    private String nickname;
+    private Boolean isDeleted;
+    private Date createdAt;
+    private Date updatedAt;
+
+    private Integer level;
+    private Integer exp;
+
+    public MyPageInfoDto(User user){
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.isDeleted = user.getIsDeleted();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
+    public MyPageInfoDto(User user, Mandalart mandalart){
+        this(user);
+        this.exp = mandalart.getExp();
+        this.level = mandalart.getLevel();
+    }
+}
